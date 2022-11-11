2846446969fa1ecd1d53f9e215e42e8f564fad70
Fengjinlong1210 <239383772@qq.com>
2022-11-11 20:04:18 +0800

    const+assert

--- 11.11练习/11.11练习.c
+++ 11.11练习/11.11练习.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#include<assert.h>
 
 //1.assert的用法(断言)  评估表达式是否正确，如果错误，返回诊断信息同时中断程序
 // 头文件<assert.h>   无返回值
@@ -43,3 +44,41 @@
 //	printf("%s\n", string_copy(arr1, arr2));//函数的链式访问，用返回值作为函数的参数
 //	return 0;
 //}
+
+
+//const的使用
+
+//int mystrlen(const char* str)		//const贴近char
+//{
+//	assert(str);
+//	int count = 0;
+//	
+//	while (*str++ != '\0')
+//		count++;
+//	
+//	return count;
+//}
+//
+//int main()
+//{
+//	//模拟实现strlen
+//	char arr[10] = "abcdefg";
+//	printf("%d\n", mystrlen(arr));
+//	return 0;
+//}
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	//int* const pa = &a;		//const修饰pa，pa的指向不能发生变化（cnost在*右侧）
+	//int* const pa = &a;			//但*pa的值可以通过地址进行改变
+	
+	const int* pa = &a;			//const在*左侧，pa的指向可以被改变
+	//*pa = 20;//错误			//pa指向的值不能被改变（const修饰*pa）
+	pa = &b;//正确
+
+	const int* const pa = &a;	//pa的指向和pa指向的值都不能被改变
+
+	return 0;
+}
