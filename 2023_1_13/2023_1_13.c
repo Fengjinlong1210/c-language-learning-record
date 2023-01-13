49db21d9de1fd61bb3f042947ac4968d2b06f3ba
Fengjinlong1210 <239383772@qq.com>
2023-01-13 21:54:16 +0800

    contacts struct

---
+++ 2023_1_13/2023_1_13.c
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "contacts.h"
+
+//创建变量
+//初始化
+
+int main()
+{
+	int input = 0;
+	contacts con;
+	init_struct(&con);
+	do
+	{
+		menu();
+		printf("请选择:\n");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			//add
+			add_contact(&con);
+			break;
+		case 2:
+			//delete
+			dele_contact(&con);
+			break;
+		case 3:
+			//search
+			search_contact(&con);
+			break;
+		case 4:
+			//sort
+			sort_contact(&con);
+			break;
+		case 5:
+			//show
+			show_contact(&con);
+			break;
+		case 6:
+			//mdfy
+			mdfy_contact(&con);
+			break;
+		case 0:
+			//exit
+			break;
+		default:
+			printf("输入错误，请重新输入\n");
+			break;
+		}
+
+	} while (input);
+		return 0;
+}
---
+++ 2023_1_13/contacts.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+#define MAX_NAME 20
+#define MAX_PHNUM 10
+#define MAX_ADDRS 20
+#define MAX 100
+
+typedef struct people
+{
+	char name[MAX_NAME];
+	int age;
+	char phnum[MAX_PHNUM];
+	char addrs[MAX_ADDRS];
+}peoinfo;
+
+typedef struct contacts
+{
+	peoinfo data[100];
+	int sz;//记录联系人个数
+}contacts;
+
+//目录
+void menu();
+
+//初始化
+void init_struct(contacts* con);
+
+//增加联系人
+void add_contact(contacts* con);
+
+//展示通讯录
+void show_contact(contacts* con);
+
+//删除指定联系人
+void dele_contact(contacts* con);
+
+//查找联系人返回坐标
+int find_contact(contacts* con, char* name);
+
+//搜索联系人
+void search_contact(contacts* con);
+
+//排序联系人
+void sort_contact(contacts* con);
+
+//比较函数
+int cmp_name(const void* p1, const void* p2);
+int cmp_age(const void* p1, const void* p2);
+
+//修改联系人信息
+void mdfy_contact(contacts* con);
+
+//清空所有联系人
+void clear_contacts(contacts* con);
